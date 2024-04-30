de8ed1e3b835f098e48a25c18dfe500c3baeb187
daniel-svendsen <daniel-svendsen@hotmail.se>
2024-04-30 12:08:41 +0200

    deleted disfunctional code

--- UXProjekt/src/views/HomeView.vue
+++ UXProjekt/src/views/HomeView.vue
@@ -1,34 +1,15 @@
 <template>
   <div class="home">
-    <!-- UsualWeatherData component -->
-    <UsualWeatherData :usualWeatherData="usualWeatherData" />
+    <h1>This is a home page</h1>
   </div>
 </template>
 
-<script>
-import UsualWeatherData from '../components/UsualWeatherData.vue'; // Importera UsualWeatherData-komponenten här
-
-export default {
-  components: {
-    UsualWeatherData // Registrera UsualWeatherData-komponenten här
-  },
-  data() {
-    return {
-      usualWeatherData: [] // Se till att usualWeatherData är definierad i din data() funktion
-    };
-  },
-  // Övrig kod
-};
-</script>
-
 <style>
 @media (min-width: 1024px) {
-  .home {
+  .about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
 }
-
-/* Övriga stilar */
-</style>../components/DataBySpecificTime.vue
+</style>
